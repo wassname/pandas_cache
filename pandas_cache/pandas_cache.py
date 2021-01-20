06530db0991b7af7a23eaa13e8dce4b7a2f856c2
wassname <wassname@users.noreply.github.com>
2021-01-21 06:59:53 +0800

    tidy

--- pandas_cache/pandas_cache.py
+++ pandas_cache/pandas_cache.py
@@ -3,25 +3,20 @@ Implements on disk caching of transformed dataframes
 
 Used on a function that returns a single pandas object,
 this decorator will execute the function, cache the dataframe as a pickle
-file using the hash of the raw code as a unique identifier.
-The next time the function runs, if the hash of the raw code matches
-what is on disk, the decoratored function will simply load and return
+file using the hash of function and subdirectory, and the arguments and filename.
+The next time the function runs, if the hashes match what is on disk, the decoratored function will simply load and return
 the pickled pandas object.
+
 This can result in speedups of 10 to 100 times, or more, depending on the
 complexity of the function that creates the dataframe.
 
-If the function changes since the last hash, this decorator will automatically delete
-the previously cached pickle and save a new one, preventing disk pollutionself.
-The caveat is that if the function name changes or the function is deleted, previously
-cached dataframes will remain on disk. For this purpose there is a 'del_cached' function
-that is will simply delete any objects cached by this decorator.
+The caveat is that previously cached dataframes will remain on disk.
 """
 
 from functools import wraps
 
 import pandas as pd
 import pickle
-import os
 import hashlib
 import inspect
 from pathlib import Path
@@ -29,44 +24,47 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def md5hash(s: str) -> str:
     return hashlib.md5(s).hexdigest()
 
-def source_code(func):
-    return ''.join(inspect.getsourcelines(func)[0])
 
-def pd_cache(cache_base=Path('.pd_cache'), use_code=True):
+def source_code(func) -> str:
+    return "".join(inspect.getsourcelines(func)[0])
+
+
+def pd_cache(cache_base: Path = Path(".pd_cache"), use_code: bool = True):
     def _pd_cache(func):
         @wraps(func)
         def cache(*args, **kw):
-            # The subdirectory contains function name and optionally code
+            # The subdirectory contains hahs of function name (and optionally code)
             if use_code:
-                f_hash = md5hash(source_code(func).encode('utf-8'))[:6]
+                f_hash = md5hash(source_code(func).encode("utf-8"))[:6]
             else:
                 f_hash = "-1"
-            cache_dir = cache_base / f'{func.__name__}_{f_hash}'
+            cache_dir = cache_base / f"{func.__name__}_{f_hash}"
             if not cache_dir.exists():
                 cache_dir.mkdir(exist_ok=True, parents=True)
-                logger.info(f'created `{cache_dir}` dir')
+                logger.info(f"created `{cache_dir}` dir")
 
-            # The file name contains the has of functions args and kwargs
-            key = (pickle.dumps(args, 1)+pickle.dumps(kw, 1))
+            # The file name contains the hash of functions args and kwargs
+            key = pickle.dumps(args, 1) + pickle.dumps(kw, 1)
             hsh = md5hash(key)[:6]
 
-            f = cache_dir/f'{hsh}.pkl.gz'
+            f = cache_dir / f"{hsh}.pkl.gz"
 
             if f.exists():
                 df = pd.read_pickle(f)
-                logger.debug(f'\t | read {f}')
+                logger.debug(f"\t | read {f}")
                 return df
 
             else:
                 # Write new
                 df = func(*args, **kw)
                 df.to_pickle(f)
-                logger.debug(f'\t | wrote {f}')
+                logger.debug(f"\t | wrote {f}")
                 return df
 
         return cache
-    return _pd_cache
 
+    return _pd_cache
